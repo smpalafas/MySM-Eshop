65afb55e9ed5e34dcc4000597748fc417b660cf2
Stefanos Balafas <smpalafas@yahoo.com>
2025-05-05 12:50:21 +0300

    inital commit

--- app/src/main/java/com/example/myeshop/ShopActivity.java
+++ app/src/main/java/com/example/myeshop/ShopActivity.java
@@ -1,7 +1,11 @@
 package com.example.myeshop;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
@@ -18,6 +22,11 @@ public class ShopActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
 
+        // Ρύθμιση του Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Κατάστημα");
+
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -26,5 +35,24 @@ public class ShopActivity extends AppCompatActivity {
 
         adapter = new ShopProductAdapter(productList, this);
         recyclerView.setAdapter(adapter);
+    }
+
+    // Δημιουργία του μενού
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_cart, menu);
+        return true;
+    }
+
+    // Χειρισμός των επιλογών του μενού
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_cart) {
+            // Άνοιγμα της οθόνης του καλαθιού
+            Intent intent = new Intent(this, CartDetailsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
